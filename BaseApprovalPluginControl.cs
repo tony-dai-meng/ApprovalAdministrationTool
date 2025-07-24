4e15ab28e9e3d0bbe8a7d754c240c805f8c1b755
Tony Dai <tony.dai@excopartners.com>
2025-07-24 16:19:14 +1000

    Add project files.

---
+++ BaseApprovalPluginControl.cs
@@ -0,0 +1,175 @@
+﻿using McTools.Xrm.Connection;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Deployment;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Activities.Presentation.Metadata;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+
+namespace ApprovalAdministrationTool
+{
+    public partial class BaseApprovalPluginControl : PluginControlBase
+    {
+        private Settings mySettings;
+
+        public BaseApprovalPluginControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MyPluginControl_Load(object sender, EventArgs e)
+        {
+            ExecuteMethod(WhoAmI);
+            ShowInfoNotification("This is a notification that can lead to XrmToolBox repository", new Uri("https://github.com/MscrmTools/XrmToolBox"));
+
+            // Loads or creates the settings for the plugin
+            if (!SettingsManager.Instance.TryLoad(GetType(), out mySettings))
+            {
+                //mySettings = new Settings();
+                //LogWarning("Settings not found => a new settings file has been created!");
+            }
+            else
+            {
+                //LogInfo("Settings found and loaded");
+            }
+        }
+
+        private void WhoAmI()
+        {
+            Service.Execute(new WhoAmIRequest());
+        }
+
+        private void tsbClose_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+        private void tsbSample_Click(object sender, EventArgs e)
+        {
+            // The ExecuteMethod method handles connecting to an
+            // organization if XrmToolBox is not yet connected
+            ExecuteMethod(GetAccounts);
+        }
+
+        private void GetAccounts()
+        {
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Getting approvals",
+                Work = (worker, args) =>
+                {
+                    args.Result = Service.RetrieveMultiple(new QueryExpression("msdyn_flow_approval") { });
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error retriving approvals", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var result = args.Result as EntityCollection;
+                    if (result != null)
+                    {
+                        MessageBox.Show($"Found {result.Entities.Count} accounts");
+
+                    }
+                }
+            });
+        }
+
+        /// <summary>
+        /// This method converts an Entity Collection object to a DataTable object.
+        /// </summary>
+        /// <param name="entityCollection">The entity collection object</param>
+        /// <returns></returns>
+        public static DataTable GetDataTableFromEntityCollection(EntityCollection entityCollection)
+        {
+            // Init datatable
+            DataTable dataTable = new DataTable("Entities");
+            //grab first entity to get attributes
+            Microsoft.Xrm.Sdk.AttributeCollection attributes = entityCollection.Entities.First().Attributes;
+
+            // Add columns to the DataTable based on the attributes of the first entity
+            if (entityCollection.Entities.Count > 0)
+            {
+                foreach (KeyValuePair<string, object> attribute in attributes)
+                {
+                    dataTable.Columns.Add(attribute.Key, attribute.Value.GetType());
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+            // Add the records in the datatable
+            foreach (Entity record in entityCollection.Entities)
+            {
+                DataRow row = dataTable.NewRow();
+                foreach (KeyValuePair<string, object> attribute in record.Attributes)
+                {
+                    row[attribute.Key] = attribute.Value;
+                }
+                dataTable.Rows.Add(row);
+            }
+            return dataTable;
+        }
+
+        /// <summary>
+        /// This event occurs when the plugin is closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyPluginControl_OnCloseTool(object sender, EventArgs e)
+        {
+            // Before leaving, save the settings
+            SettingsManager.Instance.Save(GetType(), mySettings);
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+
+            if (mySettings != null && detail != null)
+            {
+                mySettings.LastUsedOrganizationWebappUrl = detail.WebApplicationUrl;
+                LogInfo("Connection has changed to: {0}", detail.WebApplicationUrl);
+            }
+        }
+
+        private void toolStripMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        // This method is called when the control is resized.
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            var WindowWidth = this.Width;
+            var WindowHeight = this.Height;
+
+        }
+    }
+}
---
+++ BaseApprovalPluginControl.designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ApprovalAdministrationTool
+{
+    partial class BaseApprovalPluginControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tsbClose = new System.Windows.Forms.ToolStripButton();
+            this.tssSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbSample = new System.Windows.Forms.ToolStripButton();
+            this.ApprovalGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webResourceContainer = new System.Windows.Forms.WebBrowser();
+            this.toolStripMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ApprovalGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbClose,
+            this.tssSeparator1,
+            this.tsbSample});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Size = new System.Drawing.Size(732, 25);
+            this.toolStripMenu.TabIndex = 4;
+            this.toolStripMenu.Text = "toolStrip1";
+            this.toolStripMenu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStripMenu_ItemClicked);
+            // 
+            // tsbClose
+            // 
+            this.tsbClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbClose.Name = "tsbClose";
+            this.tsbClose.Size = new System.Drawing.Size(86, 22);
+            this.tsbClose.Text = "Close this tool";
+            this.tsbClose.Click += new System.EventHandler(this.tsbClose_Click);
+            // 
+            // tssSeparator1
+            // 
+            this.tssSeparator1.Name = "tssSeparator1";
+            this.tssSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsbSample
+            // 
+            this.tsbSample.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbSample.Name = "tsbSample";
+            this.tsbSample.Size = new System.Drawing.Size(47, 22);
+            this.tsbSample.Text = "Try me";
+            this.tsbSample.Click += new System.EventHandler(this.tsbSample_Click);
+            // 
+            // ApprovalGridView
+            // 
+            this.ApprovalGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.ApprovalGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ApprovalGridView.Location = new System.Drawing.Point(7, 78);
+            this.ApprovalGridView.Margin = new System.Windows.Forms.Padding(2);
+            this.ApprovalGridView.Name = "ApprovalGridView";
+            this.ApprovalGridView.RowHeadersWidth = 51;
+            this.ApprovalGridView.RowTemplate.Height = 24;
+            this.ApprovalGridView.Size = new System.Drawing.Size(338, 519);
+            this.ApprovalGridView.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(4, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Type of Approval: ";
+            // 
+            // webResourceContainer
+            // 
+            this.webResourceContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webResourceContainer.Location = new System.Drawing.Point(349, 78);
+            this.webResourceContainer.Margin = new System.Windows.Forms.Padding(2);
+            this.webResourceContainer.MinimumSize = new System.Drawing.Size(15, 16);
+            this.webResourceContainer.Name = "webResourceContainer";
+            this.webResourceContainer.Size = new System.Drawing.Size(365, 239);
+            this.webResourceContainer.TabIndex = 7;
+            this.webResourceContainer.Url = new System.Uri("https://www.google.com", System.UriKind.Absolute);
+            this.webResourceContainer.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // BaseApprovalPluginControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.webResourceContainer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ApprovalGridView);
+            this.Controls.Add(this.toolStripMenu);
+            this.Name = "BaseApprovalPluginControl";
+            this.Size = new System.Drawing.Size(732, 600);
+            this.Load += new System.EventHandler(this.MyPluginControl_Load);
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ApprovalGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripButton tsbSample;
+        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
+        private System.Windows.Forms.DataGridView ApprovalGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webResourceContainer;
+    }
+}
